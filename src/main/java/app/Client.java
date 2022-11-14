767c222b6781d303da4329251ab4954c54b44345
yudavinchi <davidyu@edu.hac.ac.il>
2022-11-14 22:28:13 +0200

    more builders

--- src/main/java/app/Client.java
+++ src/main/java/app/Client.java
@@ -4,11 +4,30 @@ import app.entities.User;
 import app.utils.File;
 import app.entities.Vehicle;
 
+import java.awt.font.FontRenderContext;
 import java.util.*;
 
 public class Client {
     public static void main(String args[]) throws Exception {
 
-        ExecuteQuery.updatePropertyById(2, "model", "hyundai", Vehicle.class);
+//        List<Vehicle> vehicles = ExecuteQuery.readByProperty("model", "honda", Vehicle.class);
+//
+//        for(Vehicle vehicle : vehicles){
+//            System.out.println(vehicle.toString());
+//        }
+//
+//        Vehicle vehicle = ExecuteQuery.readById(32111, Vehicle.class);
+//        System.out.println(vehicle.toString());
+
+//        Vehicle vehicle = new Vehicle("honda", "civic", 100, 2022, "good");
+//        ExecuteQuery.addOne(vehicle, Vehicle.class);
+
+//        List<Vehicle> vehicles = ExecuteQuery.readAll(Vehicle.class);
+//
+//        for (Vehicle vehicle : vehicles) {
+//            System.out.println(vehicle.toString());
+//        }
+
+//        ExecuteQuery.updatePropertyById(1, "model", "seat", Vehicle.class);
     }
 }
