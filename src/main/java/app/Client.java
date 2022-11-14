98cd63e1fcf4a239ce61a3aae2aa945b0c775a29
yudavinchi <davidyu@edu.hac.ac.il>
2022-11-14 09:30:17 +0200

    configurations

--- src/main/java/app/Client.java
+++ src/main/java/app/Client.java
@@ -1,13 +1,19 @@
 package app;
 
+import app.utils.File;
+
 import java.sql.*;
+import java.util.HashMap;
 
 public class Client {
     public static void main(String args[]) throws Exception {
         try {
+
+            HashMap<String, String> configurations = File.readJson("src/main/java/configurations/sql config.json");
+
             Class.forName("com.mysql.jdbc.Driver");
             Connection con = DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/orm_exercise", "root", "password");
+                    configurations.get("url"), configurations.get("user"), configurations.get("password"));
 
             Statement stmt = con.createStatement();
             ResultSet rs = stmt.executeQuery("select * from random_table");
